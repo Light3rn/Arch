7250e3e23f96c2fab5ee995ce9179ae8933a4d8d
Light3rn <31665284+lightern@users.noreply.github.com>
2017-11-06 20:01:01 +0200

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -2,6 +2,7 @@ import XMonad
 import XMonad.Config.Desktop
 
 main = xmonad desktopConfig
-    { terminal    = "xterm"
-    , modMask     = mod4Mask
+    { terminal              = "xterm"
+    , modMask               = mod4Mask
+    , focusedBorderColor    = "#1793D1"
     }
