d95978082c847053196289da696d1d9721ebf81f
Light3rn <31665284+lightern@users.noreply.github.com>
2017-11-08 10:33:38 +0200

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,9 @@
 import XMonad
+import XMonad.Hooks.ManageDocks
 
 main = xmonad def
     { terminal              = "xterm"
     , modMask               = mod4Mask
     , focusedBorderColor    = "#1793D1"
+    , manageHook = manageHook defaultConfig <+> manageDocks
     }
